baf33f33891aeadd97330bd5d6cacca1ea06910c
PhamPhong137 <pp072003@gmail.com>
2025-01-16 15:19:39 +0700

    Add api list all

--- src/index.ts
+++ src/index.ts
@@ -6,25 +6,36 @@ export interface Env {
 
 const app = new Hono<{ Bindings: Env }>();
 
-// Định nghĩa endpoint
 app.get('/api/beverages', async (c) => {
   const db = c.env.DB;
   
   try {
-    // Truy vấn D1 database
     const { results } = await db
       .prepare('SELECT * FROM Customers WHERE CompanyName = ?')
       .bind('Bs Beverages')
       .all();
 
-    return c.json(results); // Trả về kết quả dạng JSON
+    return c.json(results); 
   } catch (error) {
     console.error(error);
     return c.text('Failed to fetch data', 500);
   }
 });
+app.get('/api/listall', async (c) => {
+	const db = c.env.DB;
+	
+	try {
+	  const { results } = await db
+		.prepare('SELECT * FROM Customers')
+		.all();
+  
+	  return c.json(results); 
+	} catch (error) {
+	  console.error(error);
+	  return c.text('Failed to fetch data', 500);
+	}
+  });
 
-// Endpoint mặc định
 app.all('*', (c) => c.text('Call /api/beverages to see everyone who works at Bs Beverages'));
 
 export default app;
